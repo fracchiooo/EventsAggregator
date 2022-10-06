964d42713eb096cde025c3f3f8ebfd547e77dea4
fracchiooo <frasconocchia1@gmail.com>
2022-10-06 17:01:00 +0200

    updates

--- app/controllers/like_comments_controller.rb
+++ app/controllers/like_comments_controller.rb
@@ -1,22 +1,25 @@
 class LikeCommentsController < ApplicationController
 
     def create
+
         @like_comment = current_user.like_comments.new(like_c_params)
         if !@like_comment.save
-            flash[:notice]= @like_comment.errors.full_messages.to_sentence
+        flash[:notice]= @like_comment.errors.full_messages.to_sentence
         end
 
         #redirect_to @like_comment.comment  # dovrò mettere il redirect all evento a cui appartiene il commento
-        redirect_to root_path
+        #redirect_to root_path(param_1: "#{@like_comment.comment_id}")
 
     end
 
     def destroy
+
         @like_comment= current_user.like_comments.find(params[:id]) #dovrò inserire un nuovo modo per passargli l id del commento
         comment = @like_comment.comment # dovrò mettere l evento al posto del commento
         @like_comment.destroy
-        #redirect_to comment
-        redirect_to root_path
+        #redirect_to root_path(param_1: "#{comment.id}")
+
+
 
     end
 
