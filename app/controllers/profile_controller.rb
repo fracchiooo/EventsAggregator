d16c72f3887eb78eec7197216a413dab44b7a667
ersi <regolliersi@gmail.com>
2022-11-01 12:21:57 +0100

    Reso profili accessibili solo da utenti registrati

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,9 @@
 class ProfileController < ApplicationController
   def show
+    if !user_signed_in?
+      return redirect_to new_user_session_path
+    end
+
     begin
       @user = User.find(params[:id])
       @favorites = Favorite.where(user: @user)
